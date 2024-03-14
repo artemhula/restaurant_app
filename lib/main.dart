108b98ec8814f5597b2da802aba58f6e2989997f
artemhula <artemgul4@gmail.com>
2024-03-14 21:53:11 +0200

    Update imports and dependencies in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:restaurant/features/auth/presentation/hello_screen.dart';
-import 'firebase_options.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurant/features/auth/presentation/bloc/auth_cubit/auth_cubit.dart';
+import 'package:restaurant/features/auth/presentation/views/hello_screen.dart';
+import 'package:restaurant/locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await initializeDependencies();
   runApp(const MyApp());
 }
 
@@ -16,37 +15,42 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          textTheme: const TextTheme(
-            labelLarge: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w300,
+    return BlocProvider(
+      create: (context) => sl<AuthCubit>(),
+      child: MaterialApp(
+        theme: ThemeData(
+            textTheme: const TextTheme(
+              labelLarge: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w300,
+              ),
+              labelMedium: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                color: Color.fromARGB(255, 20, 101, 0),
+              ),
+              headlineSmall:
+                  TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              headlineMedium:
+                  TextStyle(fontSize: 26, fontWeight: FontWeight.w800),
+              headlineLarge:
+                  TextStyle(fontSize: 32, fontWeight: FontWeight.w900),
             ),
-            labelMedium: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-              color: Color.fromARGB(255, 20, 101, 0),
+            scaffoldBackgroundColor: Colors.black,
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Color.fromARGB(255, 21, 105, 0),
+              foregroundColor: Colors.white,
             ),
-            headlineSmall: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-            headlineMedium: TextStyle(fontSize: 26, fontWeight: FontWeight.w800),
-
-            headlineLarge:  TextStyle(fontSize: 32, fontWeight: FontWeight.w900),
-          ),
-          scaffoldBackgroundColor: Colors.black,
-          floatingActionButtonTheme: const FloatingActionButtonThemeData(
-            backgroundColor: Color.fromARGB(255, 21, 105, 0),
-            foregroundColor: Colors.white,
-          ),
-          colorScheme: const ColorScheme.dark(
-              primary: Color.fromARGB(255, 21, 105, 0),
-              secondary: Colors.white12,
-              tertiary: Colors.grey),
-          bottomAppBarTheme: const BottomAppBarTheme(
-            color: Color.fromARGB(255, 14, 14, 14),
-          )),
-      home: const AuthScreen(),
-      debugShowCheckedModeBanner: false,
+            colorScheme: const ColorScheme.dark(
+                primary: Color.fromARGB(255, 21, 105, 0),
+                secondary: Colors.white12,
+                tertiary: Colors.grey),
+            bottomAppBarTheme: const BottomAppBarTheme(
+              color: Color.fromARGB(255, 14, 14, 14),
+            )),
+        home: const AuthScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
