22423d2838ec4515dc989054cc2915c6b61b230b
artemhula <artemgul4@gmail.com>
2024-03-19 18:34:06 +0200

    Refactor User and UserEntity classes

--- lib/features/auth/data/models/user.dart
+++ lib/features/auth/data/models/user.dart
@@ -1,3 +1,24 @@
 import 'package:restaurant/features/auth/domain/entity/user.dart';
 
-class User extends UserEntity {}
+class UserModel extends UserEntity {
+  const UserModel({
+    required super.uid,
+    required super.name,
+    required super.birthday,
+    required super.sex,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'uid': uid,
+        'name': name,
+        'birthday': birthday,
+        'sex': sex,
+      };
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        uid: json['uid'],
+        name: json['name'],
+        birthday: json['birthday'],
+        sex: json['sex'],
+      );
+}
--- lib/features/auth/domain/entity/user.dart
+++ lib/features/auth/domain/entity/user.dart
@@ -1,3 +1,19 @@
-class UserEntity {
+import 'package:equatable/equatable.dart';
 
+class UserEntity extends Equatable {
+  final String uid;
+  final String name;
+
+  final DateTime birthday;
+  final bool sex;
+
+  const UserEntity(
+      {required this.uid,
+      required this.name,
+
+      required this.birthday,
+      required this.sex});
+
+  @override
+  List<Object?> get props => [uid, name, birthday, sex];
 }
