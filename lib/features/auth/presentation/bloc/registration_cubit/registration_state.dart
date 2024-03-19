da6911298f66539f90a6f544451f838853b6f3e3
artemhula <artemgul4@gmail.com>
2024-03-19 18:31:17 +0200

    Update registration cubit

---
+++ lib/features/auth/presentation/bloc/registration_cubit/registration_state.dart
@@ -0,0 +1,20 @@
+part of 'registration_cubit.dart';
+
+sealed class RegistrationState extends Equatable {
+  const RegistrationState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class RegistrationInitial extends RegistrationState {}
+
+final class RegistrationLoading extends RegistrationState {}
+
+final class UserIsRegistered extends RegistrationState {}
+
+final class UserIsNotRegistered extends RegistrationState {}
+
+final class RegistrationSuccessful extends RegistrationState {}
+
+final class RegistrationFailure extends RegistrationState {}
