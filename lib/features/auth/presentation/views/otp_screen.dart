4782d705b2d809f401a584ba80a821d7f9c331eb
artemhula <artemgul4@gmail.com>
2024-03-19 18:34:40 +0200

    Add registration functionality to screens

--- lib/features/auth/presentation/views/otp_screen.dart
+++ lib/features/auth/presentation/views/otp_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pinput/pinput.dart';
 import 'package:restaurant/features/auth/presentation/bloc/auth_cubit/auth_cubit.dart';
+import 'package:restaurant/features/auth/presentation/bloc/registration_cubit/registration_cubit.dart';
+import 'package:restaurant/features/auth/presentation/views/registration_screen.dart';
 import 'package:restaurant/home_screen.dart';
 
 class OTPScreen extends StatelessWidget {
@@ -21,74 +23,90 @@ class OTPScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: BlocBuilder<AuthCubit, AuthState>(
-          builder: (context, state) {
-            if (state is AuthLoading) {
-              return Center(
-                child: CircularProgressIndicator(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-              );
+        child: BlocListener<RegistrationCubit, RegistrationState>(
+          listener: (context, state) {
+            if (state is UserIsRegistered) {
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                );
+              });
+            } else if (state is UserIsNotRegistered) {
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegistrationScreen()),
+                );
+              });
             }
-            if (state is OtpSended) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Введіть код',
-                    textAlign: TextAlign.left,
-                    style: Theme.of(context).textTheme.headlineLarge,
+          },
+          child: BlocBuilder<AuthCubit, AuthState>(
+            builder: (context, state) {
+              if (state is AuthLoading) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: Theme.of(context).colorScheme.primary,
                   ),
-                  const SizedBox(height: 20),
-                  Text(
-                    'Ми надіcлали SMS-повідомлення на номер ${state.phoneNumber}',
-                    textAlign: TextAlign.left,
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  const SizedBox(height: 20),
-                  Center(
-                    child: SizedBox(
-                      height: 64,
-                      child: Pinput(
-                        length: 6,
-                        onCompleted: (pin) {
-                          context.read<AuthCubit>().verifyCode(smsCode: pin);
-                        },
-                        keyboardType: TextInputType.number,
-                        defaultPinTheme: defaultPinTheme,
-                        focusedPinTheme: defaultPinTheme.copyWith(
-                          width: 60,
-                          height: 64,
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.secondary,
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all(
-                                color: Theme.of(context).colorScheme.primary),
+                );
+              }
+              if (state is OtpSended) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Введіть код',
+                      textAlign: TextAlign.left,
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Ми надіcлали SMS-повідомлення на номер ${state.phoneNumber}',
+                      textAlign: TextAlign.left,
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: SizedBox(
+                        height: 64,
+                        child: Pinput(
+                          length: 6,
+                          onCompleted: (pin) {
+                            context.read<AuthCubit>().verifyCode(smsCode: pin);
+                          },
+                          keyboardType: TextInputType.number,
+                          defaultPinTheme: defaultPinTheme,
+                          focusedPinTheme: defaultPinTheme.copyWith(
+                            width: 60,
+                            height: 64,
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.secondary,
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(
+                                  color: Theme.of(context).colorScheme.primary),
+                            ),
                           ),
                         ),
                       ),
                     ),
+                  ],
+                );
+              }
+              if (state is OtpVerified) {
+                context.read<RegistrationCubit>().checkIsUserRegistered();
+              }
+              if (state is AuthFailure) {
+                return Center(
+                  child: Text(
+                    state.message,
+                    style: Theme.of(context).textTheme.headlineSmall,
                   ),
-                ],
-              );
-            }
-            if (state is OtpVerified) {
-              WidgetsBinding.instance.addPostFrameCallback((_) {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HomeScreen()));
-              });
-            }
-            if (state is AuthFailure) {
-              return Center(
-                child: Text(
-                  state.message,
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-              );
-            }
-            return Container();
-          },
+                );
+              }
+              return Container();
+            },
+          ),
         ),
       ),
     );
--- lib/features/auth/presentation/views/registration_screen.dart
+++ lib/features/auth/presentation/views/registration_screen.dart
@@ -1,145 +1,176 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurant/features/auth/presentation/bloc/registration_cubit/registration_cubit.dart';
+import 'package:restaurant/home_screen.dart';
+import 'package:restaurant/views/main_screen.dart';
 
-class RegisterScreen extends StatefulWidget {
-  const RegisterScreen({super.key});
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
 
   @override
-  State<RegisterScreen> createState() => _RegisterScreenState();
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
 }
 
-class _RegisterScreenState extends State<RegisterScreen> {
+class _RegistrationScreenState extends State<RegistrationScreen> {
   String name = '';
   DateTime birthday = DateTime.now();
-  bool isMan = true;
+  bool sex = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Давайте ми уточнимо дані про вас',
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-              const SizedBox(height: 40),
-              TextFormField(
-                onChanged: (value) {
-                  name = value;
-                },
-                inputFormatters: [
-                  FilteringTextInputFormatter.singleLineFormatter
-                ],
-                style: Theme.of(context).textTheme.headlineSmall,
-                keyboardType: TextInputType.name,
-                decoration: InputDecoration(
-                  hintText: 'Ім\'я',
-                  contentPadding: const EdgeInsets.all(20),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
+      body: BlocBuilder<RegistrationCubit, RegistrationState>(
+        builder: (context, state) {
+          if (state is RegistrationLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (state is UserIsNotRegistered) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Давайте ми уточнимо дані про вас',
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                    const SizedBox(height: 40),
+                    TextFormField(
+                      onChanged: (value) {
+                        name = value;
+                      },
+                      inputFormatters: [
+                        FilteringTextInputFormatter.singleLineFormatter
+                      ],
+                      style: Theme.of(context).textTheme.headlineSmall,
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        hintText: 'Ім\'я',
+                        contentPadding: const EdgeInsets.all(20),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Дата народження\n${birthday.day}/${birthday.month}/${birthday.year}',
+                          style: Theme.of(context).textTheme.headlineMedium,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() async {
+                              DateTime? pickedBirthday = await showDatePicker(
+                                context: context,
+                                firstDate: DateTime(1924),
+                                lastDate: birthday,
+                                currentDate: birthday,
+                              );
+                              if (pickedBirthday != null) {
+                                setState(() {
+                                  birthday = pickedBirthday;
+                                });
+                              }
+                            });
+                          },
+                          style: TextButton.styleFrom(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.secondary,
+                            foregroundColor: Colors.white,
+                            fixedSize: Size(
+                              MediaQuery.of(context).size.width * 0.3,
+                              MediaQuery.of(context).size.height * 0.055,
+                            ),
+                          ),
+                          child: const Text('Обрати'),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 25),
+                    Text('Стать',
+                        style: Theme.of(context).textTheme.headlineMedium),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ListTile(
+                            title: const Text('Чоловіча'),
+                            leading: Radio<bool>(
+                              value: true,
+                              groupValue: sex,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  if (value != null) {
+                                    sex = true;
+                                  }
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: ListTile(
+                            title: const Text('Жіноча'),
+                            leading: Radio<bool>(
+                              value: false,
+                              groupValue: sex,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  if (value != null) {
+                                    sex = false;
+                                  }
+                                });
+                              },
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          context
+                              .read<RegistrationCubit>()
+                              .register(name, sex, birthday);
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primary,
+                          foregroundColor: Colors.white,
+                          fixedSize: Size(
+                            MediaQuery.of(context).size.width * 0.3,
+                            MediaQuery.of(context).size.height * 0.055,
+                          ),
+                        ),
+                        child: Text(
+                          'Готово',
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-              const SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Дата народження\n${birthday.day}/${birthday.month}/${birthday.year}',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      setState(() async {
-                        DateTime? pickedBirthday = await showDatePicker(
-                          context: context,
-                          firstDate: DateTime(1924),
-                          lastDate: birthday,
-                          currentDate: birthday,
-                        );
-                        if (pickedBirthday != null) {
-                          setState(() {
-                            birthday = pickedBirthday;
-                          });
-                        }
-                      });
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Theme.of(context).colorScheme.secondary,
-                      foregroundColor: Colors.white,
-                      fixedSize: Size(
-                        MediaQuery.of(context).size.width * 0.3,
-                        MediaQuery.of(context).size.height * 0.055,
-                      ),
-                    ),
-                    child: const Text('Обрати'),
-                  ),
-                ],
+            );
+          }
+          if (state is RegistrationSuccessful) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HomeScreen(),
               ),
-              const SizedBox(height: 25),
-              Text('Стать', style: Theme.of(context).textTheme.headlineMedium),
-              Row(
-                children: [
-                  Expanded(
-                    child: ListTile(
-                      title: const Text('Чоловіча'),
-                      leading: Radio<bool>(
-                        value: true,
-                        groupValue: isMan,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            if (value != null) {
-                              isMan = true;
-                            }
-                          });
-                        },
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: ListTile(
-                      title: const Text('Жіноча'),
-                      leading: Radio<bool>(
-                        value: false,
-                        groupValue: isMan,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            if (value != null) {
-                              isMan = false;
-                            }
-                          });
-                        },
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    foregroundColor: Colors.white,
-                    fixedSize: Size(
-                      MediaQuery.of(context).size.width * 0.3,
-                      MediaQuery.of(context).size.height * 0.055,
-                    ),
-                  ),
-                  child: Text(
-                    'Готово',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
+            );
+          }
+          if (state is RegistrationFailure) {
+            return const Center(child: Text('oh no!'));
+          }
+          return Container();
+        },
       ),
     );
   }
