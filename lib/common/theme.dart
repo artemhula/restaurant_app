f3880ab7f289d47df022b0dbfe8b8b50a5addaa5
artemhula <artemgul4@gmail.com>
2024-04-13 23:04:58 +0300

    Changed theme

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -3,6 +3,7 @@ import 'package:pinput/pinput.dart';
 
 final theme = ThemeData(
   textTheme: const TextTheme(
+    bodySmall: TextStyle(fontSize: 10),
     labelLarge: TextStyle(
       fontSize: 18,
       fontWeight: FontWeight.w300,
@@ -10,20 +11,20 @@ final theme = ThemeData(
     labelMedium: TextStyle(
       fontSize: 19,
       fontWeight: FontWeight.w400,
-      color: Color.fromARGB(255, 20, 101, 0),
+      color: Color.fromARGB(255, 101, 0, 89),
     ),
-    headlineSmall: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+    headlineSmall: TextStyle(fontSize: 19, fontWeight: FontWeight.w500),
     headlineMedium: TextStyle(fontSize: 23, fontWeight: FontWeight.w700),
     headlineLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.w900),
   ),
   scaffoldBackgroundColor: Colors.black,
   floatingActionButtonTheme: const FloatingActionButtonThemeData(
-    backgroundColor: Color.fromARGB(255, 21, 105, 0),
+    backgroundColor: Color.fromARGB(255, 105, 0, 96),
     foregroundColor: Colors.white,
   ),
   colorScheme: const ColorScheme.dark(
-      primary: Color.fromARGB(255, 21, 105, 0),
-      secondary: Colors.white12,
+      primary: Color.fromARGB(255, 105, 0, 100),
+      secondary: Color.fromARGB(13, 255, 255, 255),
       tertiary: Colors.grey),
   bottomAppBarTheme: const BottomAppBarTheme(
     color: Color.fromARGB(255, 14, 14, 14),
@@ -36,6 +37,6 @@ final defaultPinTheme = PinTheme(
   decoration: BoxDecoration(
     color: theme.colorScheme.secondary,
     borderRadius: BorderRadius.circular(8),
-    border: Border.all(color: Colors.transparent),
+    border: Border.all(color: theme.colorScheme.secondary),
   ),
 );
