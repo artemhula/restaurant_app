fc3ab4c399f65f864259f0294d3cf03990cb344c
Artem Hula <artemgul4@gmail.com>
2024-05-08 12:21:53 +0300

    Update tab names

--- lib/utils/tabs.dart
+++ lib/utils/tabs.dart
@@ -1,34 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant/features/menu/presentation/widgets/product.dart';
 
 const tabs = [
   Tab(child: Text('Роли')),
   Tab(child: Text('Сети')),
-  Tab(child: Text('Унагі')),
+  Tab(child: Text('Супи')),
   Tab(child: Text('Вок')),
   Tab(child: Text('Напої')),
-  Tab(child: Text('Сети')),
 ];
 
-final views = [
-  GridView.builder(
-    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-      crossAxisCount: 2,
-      childAspectRatio: 0.24 / 0.3,
-      crossAxisSpacing: 25,
-      mainAxisSpacing: 25,
-    ),
-    
-    itemBuilder: (context, index) => const Product(
-      id: 1,
-      photoUrl: '',
-      title: 'Філадельфія з огірком',
-      weight: 240,
-      price: 259,
-    ),
-  ),
-  Container(child: Text('hello3')),
-  Container(child: Text('hello4')),
-  Container(child: Text('hello5')),
-  Container(child: Text('hello6')),
-];
+
