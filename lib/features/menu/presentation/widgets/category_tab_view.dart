7b7b5ad6066954d113b153c2211e240725de1b08
Artem Hula <artemgul4@gmail.com>
2024-05-08 12:19:18 +0300

    Refactor CategoryTabView to use ProductCubit and display products by category

--- lib/features/menu/presentation/widgets/category_tab_view.dart
+++ lib/features/menu/presentation/widgets/category_tab_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant/utils/tabs.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurant/features/menu/domain/entity/category.dart';
+import 'package:restaurant/features/menu/presentation/bloc/product_cubit/product_cubit.dart';
+import 'package:restaurant/features/menu/presentation/widgets/product_card.dart';
 
 class CategoryTabView extends StatelessWidget {
   const CategoryTabView({super.key});
@@ -7,9 +10,48 @@ class CategoryTabView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: TabBarView(children: views),
+      child: BlocBuilder<ProductCubit, ProductState>(
+        bloc: context.read<ProductCubit>()..loadProducts(),
+        builder: (context, state) {
+          if (state is ProductLoading) {
+            return const CircularProgressIndicator();
+          } else if (state is ProductLoaded) {
+            return TabBarView(
+              physics: const BouncingScrollPhysics(),
+              children: Category.values.map((Category category) {
+                var productsInCategory = state.products
+                    .where((element) => element.category == category)
+                    .toList();
+                return Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.21 / 0.3,
+                      crossAxisSpacing: 25,
+                      mainAxisSpacing: 25,
+                    ),
+                    itemCount: productsInCategory.length,
+                    itemBuilder: (context, index) {
+                      return ProductCard(
+                        id: productsInCategory[index].id,
+                        photoUrl: productsInCategory[index].imageUrl,
+                        title: productsInCategory[index].name,
+                        weight: productsInCategory[index].weight,
+                        price: productsInCategory[index].price,
+                      );
+                    },
+                  ),
+                );
+              }).toList(),
+            );
+          } else if (state is ProductFailure) {
+            return Text('Error: ${state.message}');
+          } else {
+            return Container();
+          }
+        },
       ),
     );
   }
